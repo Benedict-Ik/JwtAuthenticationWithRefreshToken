8a73fca02059f8c4e2a54a18f4920c6c496d7436
Benedict Odoh <benedict.ik98@gmail.com>
2025-05-04 16:50:59 +0100

    Modified README.md

--- obj/Debug/net8.0/JwtAuthenticationWithRefreshToken.AssemblyInfo.cs
+++ obj/Debug/net8.0/JwtAuthenticationWithRefreshToken.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthenticationWithRefreshToken")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d1919f990e8c015a2355fd29444218af93d4df")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthenticationWithRefreshToken")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthenticationWithRefreshToken")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
